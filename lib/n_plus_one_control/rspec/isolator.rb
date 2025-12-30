2eeb47f90948426a344716a231452ebf5c936132
Alex Zavgorodnev <klondaiker@bk.ru>
2025-12-30 17:56:21 +0300

    Update isolator.rb

--- lib/n_plus_one_control/rspec/isolator.rb
+++ lib/n_plus_one_control/rspec/isolator.rb
@@ -1,9 +1,9 @@
 ::RSpec.configure do |config|
-  config.before(:begin, :n_plus_one) do
+  config.before(:each, :n_plus_one) do
     ::Isolator.incr_thresholds!
   end
 
-  config.after(:rollback, :n_plus_one) do
+  config.after(:each, :n_plus_one) do
     ::Isolator.decr_thresholds!
   end
 end
