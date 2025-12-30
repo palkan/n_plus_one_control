916df0ce5333d8b85a6f24c75b886e2c7d935fdd
klondaiker <klondaiker@bk.ru>
2025-12-30 17:46:24 +0300

    feat: compatibility with isolator

---
+++ lib/n_plus_one_control/rspec/isolator.rb
@@ -0,0 +1,9 @@
+RSpec.configure do |config|
+  config.before(:begin, :n_plus_one) do
+    ::Isolator.incr_thresholds!
+  end
+
+  config.after(:rollback, :n_plus_one) do
+    ::Isolator.decr_thresholds!
+  end
+end
